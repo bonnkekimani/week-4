d9f99d0ec7a88a00cb71bc320e7c8414915b2c57
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-04-04 10:59:52 +0300

    jsfile1

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 fetch("http://localhost:3000/Projects",)
     .then(res => res.json())
     .then(data => {
-        const  character = document.querySelector(".box")
+        const  character = document.querySelector(".jstyles")
         for(let i = 0; i < data.length; i++) {
             console.log(data[i]);
             let pic = data[i].image_url;
-            let cost = data[i].price;
-            let local = data[i].address;
-            let  about = data[i].description;
+            let ref = data[i].name;
+            let more = data[i].description;
+            let price = data[i].budget;
+            let area = data[i].location;
+                    
             console.log(data[i]);
             const card = document.createElement("div");
             card.classList.add("card");
             card.innerHTML +=`
             <img src="${pic}"style="width:300px; height:250px;" />
-            <h5>Price: ${cost}</h5>
-            <h5>Address: ${local}</h5>
-            <h5>Description: ${about}</h5>
+            <h5>Name: ${ref}</h5>
+            <h5>Description: ${more}</h5>
+            <h5>Budget: ${price}</h5>
+            <h5>Location: ${area}</h5>
             `
             character.appendChild(card)
             }
