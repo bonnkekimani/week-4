68e3c9f64a7358348d68db814d606cc1e00320dd
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-04-05 00:57:21 +0300

    likes

--- main.js
+++ main.js
@@ -9,8 +9,10 @@ fetch("http://localhost:3000/Projects",)
             let more = data[i].description;
             let price = data[i].budget;
             let area = data[i].location;
+            let sub = data[i].likes
                     
             console.log(data[i]);
+            // const likeButton =document.createElement("button");//add
             const card = document.createElement("div");
             card.classList.add("card");
             card.innerHTML +=`
@@ -19,9 +21,20 @@ fetch("http://localhost:3000/Projects",)
             <h5>Description: ${more}</h5>
             <h5>Budget: ${price}</h5>
             <h5>Location: ${area}</h5>
+            <button id="likeCount">Likes: ${sub}</button>
 
             `
             character.appendChild(card)
+            // likes.onclick=function(){
+            //     subs+=1;
+            // }
+            // let likeCount = document.getElementById("likeCount");
+            // sub.onclick=function(){ //Implementing Functionality
+            //     votes+=1
+            //     sub.textContent = `${votes}`
+          
+            //   }
+
 
             }
     })
